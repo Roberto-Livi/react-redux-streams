8eec9086420a3ca9b534f296533d527d92ea801a
Roberto-Livi <robertomlivi@outlook.com>
2020-09-25 14:09:05 -0400

    wiring up the google api library

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,16 @@
 import React from 'react'
 
 class GoogleAuth extends React.Component {
+
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '101642828581-ij9qj7liv604msqoonn7t3uaqpjpaqip.apps.googleusercontent.com',
+                scope: 'email'
+            })
+        })
+    }
+
     render(){
         return (
             <div>Google Auth</div>
