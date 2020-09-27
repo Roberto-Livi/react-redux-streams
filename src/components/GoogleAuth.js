eb141d8c6ee8e72aec031ddeca8afeee76c13bd9
Roberto-Livi <robertomlivi@outlook.com>
2020-09-27 03:33:57 -0400

    setting up redux and actions and reducers folders

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -22,11 +22,11 @@ class GoogleAuth extends React.Component {
         this.setState({ isSignedin: this.auth.isSignedIn.get() })
     }
 
-    onSignIn = () => {
+    onSignInClick = () => {
         this.auth.signIn()
     }
 
-    onSignOut = () => {
+    onSignOutClick = () => {
         this.auth.signOut()
     }
 
@@ -35,14 +35,14 @@ class GoogleAuth extends React.Component {
             return <div>I don't know if we are signed in</div>
         } else if (this.state.isSignedIn) {
             return (
-                <button onClick={this.onSignOut} className="ui red google button">
+                <button onClick={this.onSignOutClick} className="ui red google button">
                     <i className="google icon" />
                     Sign Out
                 </button>
             )
         } else {
             return (
-                <button onClick={this.onSignIn} className="ui red google button">
+                <button onClick={this.onSignInClick} className="ui red google button">
                     <i className="google icon" /> 
                     Sign In with Google
                 </button>
