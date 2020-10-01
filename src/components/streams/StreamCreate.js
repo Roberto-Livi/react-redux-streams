647be8114bccf6e2d4e519a5c7cee08b7b5d5ee9
Roberto-Livi <robertomlivi@outlook.com>
2020-10-01 09:52:31 -0400

    validation of form inputs

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -21,10 +21,25 @@ class StreamCreate extends React.Component {
             <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
                 <Field name="title" component={this.renderInput} label="Enter Title" />
                 <Field name="description" component={this.renderInput} label="Enter Description" />
+                <button className="ui button primary">Submit</button>
             </form>
         )
     }
     
+}
+
+const validate = (formValues) => {
+    const errors = {}
+    if(!formValues.title) {
+        // only ran if the user did not enter a title
+        errors.title = "You must enter a title"
+    }
+
+    if(!formValues.description) {
+        errors.description = "You must enter a description"
+    }
+
+    return errors
 }
 
 export default reduxForm({
