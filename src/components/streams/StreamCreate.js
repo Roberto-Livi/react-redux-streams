5833729acb32c6359ddc645af59aae5e4d9fa0ea
Roberto-Livi <robertomlivi@outlook.com>
2020-10-04 13:42:59 -0400

    creating streams through action creators using axios

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+import { createStream } from '../../actions'
 
 class StreamCreate extends React.Component {
 
@@ -16,7 +18,7 @@ class StreamCreate extends React.Component {
     }
 
     renderInput = ({ input, label, meta }) => {
-        const className = `field ${meta.error && meta.touch ? 'error' : ''}`
+        const className = `field ${meta.error && meta.touched ? `error` : ``}`
         return (
             <div className={className}>
                 <label>{label}</label>
