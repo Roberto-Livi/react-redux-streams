35d61b023cd18c4c8bfacf3bbedda3e29cc799f8
Roberto-Livi <robertomlivi@outlook.com>
2020-10-05 10:56:23 -0400

    dispatching actions after stream creation

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 import { connect } from 'react-redux'
-import { createStream } from '../../actions'
+import { createStream } from '../../actions/index'
 
 class StreamCreate extends React.Component {
 
@@ -28,7 +28,7 @@ class StreamCreate extends React.Component {
         )
     }
 
-    onSubmit = (formValues) => {
+    onSubmit = formValues => {
         this.props.createStream(formValues)
     }
 
