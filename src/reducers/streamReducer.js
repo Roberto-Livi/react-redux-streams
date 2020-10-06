3b1bb0f4385faad0e993a40d63627a2e3b688cdf
Roberto-Livi <robertomlivi@outlook.com>
2020-10-06 12:02:59 -0400

    merging a list of records using mapKeys

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -9,6 +9,9 @@ import {
 
 export default (state = [], action) => {
     switch(action.type) {
+        case FETCH_STREAMS:
+            // mapKeys is a function from lodash thats going to take an array and return an object
+            return { ...state, ..._.mapKeys(action.payload, 'id') }
         case FETCH_STREAM:
             return { ...state, [action.payload.id]: action.payload }
         case CREATE_STREAM:
